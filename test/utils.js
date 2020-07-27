3561fac8b4baba19c1075dd52ba7f2d10d0ff52b
Daniel Chiquito <daniel.chiquito@kitware.com>
2020-07-27 11:39:37 -0400

    Change test server port

--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export async function testComponent(component) {
-  await page.goto(`http://localhost:8080/#/${component}`);
+  await page.goto(`http://localhost:7922/#/${component}`);
 }
